c8118ab75b01acf413b2005b6b64a5d0591a496b
joshmorenx <joshuamoreno21@gmail.com>
2024-10-21 12:29:18 -0600

    Changed Text to TextInput in the date field

--- client/app/register.tsx
+++ client/app/register.tsx
@@ -1,10 +1,14 @@
 import { ScrollView, Platform } from "react-native";
-import { View, Text, TextInput } from 'react-native';
-import { Button } from 'react-native-paper';
+import { View, Text, TextInput, Keyboard } from "react-native";
+import { Button } from "react-native-paper";
 import { useState, useEffect } from "react";
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { useSendRegisterForm } from '@/hooks/useSendRegisterForm';
-import { btnPressedStyle, btnReleasedStyle, inputStyle } from '@/components/ThemedInputs';
+import DateTimePicker from "@react-native-community/datetimepicker";
+import { useSendRegisterForm } from "@/hooks/useSendRegisterForm";
+import {
+    btnPressedStyle,
+    btnReleasedStyle,
+    inputStyle,
+} from "@/components/ThemedInputs";
 
 export default function Register(): JSX.Element {
     const [pressed, setPressed] = useState(false);
@@ -12,14 +16,20 @@ export default function Register(): JSX.Element {
     const [date, setDate] = useState(new Date());
     const [show, setShow] = useState(false);
     const [formFilled, setFormFilled] = useState(false);
-    const { formData, msg, setMsg, handleChange, changing, sendRequest } = useSendRegisterForm({
-        fullname: '',
-        username: '',
-        date: new Date(),
-        email: '',
-        password: '',
-        confirmPassword: ''
-    }, setShow, setDateSetted, setDate);
+    const { formData, msg, setMsg, handleChange, changing, sendRequest } =
+        useSendRegisterForm(
+            {
+                fullname: "",
+                username: "",
+                date: new Date(),
+                email: "",
+                password: "",
+                confirmPassword: "",
+            },
+            setShow,
+            setDateSetted,
+            setDate
+        );
 
     const showDatepicker = (): void => {
         setShow(true);
@@ -27,7 +37,14 @@ export default function Register(): JSX.Element {
     };
 
     useEffect(() => {
-        if (formData.fullname && formData.username && formData.email && formData.password && formData.confirmPassword && dateSetted) {
+        if (
+            formData.fullname &&
+            formData.username &&
+            formData.email &&
+            formData.password &&
+            formData.confirmPassword &&
+            dateSetted
+        ) {
             setFormFilled(true);
         } else {
             setFormFilled(false);
@@ -42,13 +59,39 @@ export default function Register(): JSX.Element {
     }, [msg]);
 
     return (
-        <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ fontSize: 24, marginBottom: 10, fontWeight: "bold", textAlign: "center", color: "#0a7ea4" }}>New Register</Text>
-                <TextInput value={formData.fullname} onChange={(event) => handleChange(event, 'fullname')} placeholder="Full Name" style={inputStyle} />
-                <TextInput value={formData.username} onChange={(event) => handleChange(event, 'username')} placeholder="Username" style={inputStyle} />
+        <ScrollView
+            contentContainerStyle={{
+                flexGrow: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+                <Text
+                    style={{
+                        fontSize: 24,
+                        marginBottom: 10,
+                        fontWeight: "bold",
+                        textAlign: "center",
+                        color: "#0a7ea4",
+                    }}
+                >
+                    New Register
+                </Text>
+                <TextInput
+                    value={formData.fullname}
+                    onChange={(event) => handleChange(event, "fullname")}
+                    placeholder="Full Name"
+                    style={inputStyle}
+                />
+                <TextInput
+                    value={formData.username}
+                    onChange={(event) => handleChange(event, "username")}
+                    placeholder="Username"
+                    style={inputStyle}
+                />
                 <View>
-                    {Platform.OS === 'web' ? (
+                    {Platform.OS === "web" ? (
                         // Si estamos en web, usamos un input de tipo date de HTML5
                         <input
                             type="date"
@@ -58,21 +101,44 @@ export default function Register(): JSX.Element {
                                 changing(event, selectedDate);
                                 setDateSetted(true);
                             }}
-                            style={{ width: 350, height: 60, borderColor: "gray", borderWidth: 1, margin: 10, borderRadius: 5, color: "#767676" }}
+                            style={{
+                                width: 350,
+                                height: 60,
+                                borderColor: "gray",
+                                borderWidth: 1,
+                                margin: 10,
+                                borderRadius: 5,
+                                color: "#767676",
+                            }}
                         />
                     ) : (
                         // Si estamos en móvil, usamos el DateTimePicker de react-native
-                        <Text onPress={showDatepicker} style={{ width: 350, height: 60, borderColor: "gray", borderWidth: 1, margin: 10, padding: 10, borderRadius: 5, textAlignVertical: "center", color: "#767676" }}>
-                            {!dateSetted ? 'Select your date of birth' :
-                                date.toLocaleDateString('en-US', {
-                                    month: 'short',
-                                    day: '2-digit',
-                                    year: 'numeric'
-                                })
-                            }
-                        </Text>
+                        <TextInput
+                            onPress={showDatepicker}
+                            onFocus={Keyboard.dismiss}
+                            showSoftInputOnFocus={false}
+                            style={{
+                                width: 350,
+                                height: 60,
+                                borderColor: "gray",
+                                borderWidth: 1,
+                                margin: 10,
+                                padding: 10,
+                                borderRadius: 5,
+                                textAlignVertical: "center",
+                                color: "#767676",
+                            }}
+                        >
+                            {!dateSetted
+                                ? "Select your date of birth"
+                                : date.toLocaleDateString("en-US", {
+                                    month: "short",
+                                    day: "2-digit",
+                                    year: "numeric",
+                                })}
+                        </TextInput>
                     )}
-                    {show && Platform.OS !== 'web' && (
+                    {show && Platform.OS !== "web" && (
                         <DateTimePicker
                             value={date}
                             mode="date"
@@ -81,10 +147,39 @@ export default function Register(): JSX.Element {
                         />
                     )}
                 </View>
-                <TextInput value={formData.email} onChange={(event) => handleChange(event, 'email')} placeholder="Email" style={inputStyle} />
-                <TextInput value={formData.password} onChange={(event) => handleChange(event, 'password')} placeholder="Password" secureTextEntry={true} style={inputStyle} />
-                <TextInput value={formData.confirmPassword} onChange={(event) => handleChange(event, 'confirmPassword')} placeholder="Confirm Password" secureTextEntry={true} style={inputStyle} />
-                <Button onPress={sendRequest} mode="contained-tonal" disabled={!formFilled} style={{ marginTop: 15, ...(pressed ? btnPressedStyle : btnReleasedStyle) }} onPressIn={() => setPressed(!pressed)} onPressOut={() => setPressed(!pressed)}>Register</Button>
+                <TextInput
+                    value={formData.email}
+                    onChange={(event) => handleChange(event, "email")}
+                    placeholder="Email"
+                    style={inputStyle}
+                />
+                <TextInput
+                    value={formData.password}
+                    onChange={(event) => handleChange(event, "password")}
+                    placeholder="Password"
+                    secureTextEntry={true}
+                    style={inputStyle}
+                />
+                <TextInput
+                    value={formData.confirmPassword}
+                    onChange={(event) => handleChange(event, "confirmPassword")}
+                    placeholder="Confirm Password"
+                    secureTextEntry={true}
+                    style={inputStyle}
+                />
+                <Button
+                    onPress={sendRequest}
+                    mode="contained-tonal"
+                    disabled={!formFilled}
+                    style={{
+                        marginTop: 15,
+                        ...(pressed ? btnPressedStyle : btnReleasedStyle),
+                    }}
+                    onPressIn={() => setPressed(!pressed)}
+                    onPressOut={() => setPressed(!pressed)}
+                >
+                    Register
+                </Button>
             </View>
         </ScrollView>
     );
